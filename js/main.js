0d78d5e7a4e530aade1e0e117daff3d9984de143
Marta2Ges <ggarridomarta@gmail.com>
2021-01-28 11:07:58 +0100

    Add js changes

--- js/main.js
+++ js/main.js
@@ -1,2 +1,56 @@
+'use strict';
 
-  
+const number = document.querySelector('.js-number');
+const button = document.querySelector('.js-button');
+const track = document.querySelector('.js-pista');
+
+
+
+let max = 100; 
+
+function getRandomNumber(max) {
+    return Math.ceil(Math.random() * max);
+    }
+    let random = getRandomNumber (100);
+    console.log (random);
+
+    const result = getRandomNumber(max); 
+
+
+
+    function ContainerElements (index, message){
+        index.innerHTML = message;
+    }
+   
+
+    function Message () {
+    
+        if (number.value <= 0 || number.value > max) {
+            ContainerElements(track, 'El número debe estar entre 1 y 100');
+          } else if (number.value < result) {
+            ContainerElements(track,'Demasiado bajo');
+          } else if (number.value > result) {
+            ContainerElements(track, 'Demasiado alto');
+          } else {
+            ContainerElements(track, '¡Good job!');
+          }
+    
+        }
+
+        const tries = document.querySelector('.js-intentos');
+   
+     
+ 
+let click = 0;
+function Counter() {
+  click++;
+  if (click >= 1) {
+      tries.innerHTML = `Número de intentos: ${click}`;
+}
+}
+   function numbers (){
+    Message ();
+    Counter ();
+
+   }    
+button.addEventListener('click', numbers);
